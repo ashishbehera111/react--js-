5841bd2f12450c478e1401d6cd8357322e4ccc11
Ashish Behera <39208674+ashishbehera111@users.noreply.github.com>
2021-04-25 14:00:37 +0530

    hiii
    hii

---
+++ app.js
@@ -0,0 +1,17 @@
+import  React, {Component} from "react"
+import HEADER from "./header"
+import CONTENT from "./content"
+import FOOTER from "./footer"
+
+class name extends Component{
+    render(){
+        return(
+            <div>
+                <HEADER/>
+                <CONTENT/>
+                <FOOTER/>
+            </div>
+        )
+    }
+}
+export default name
