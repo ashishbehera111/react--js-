53e1497bcb610f29938700d1e921aa91d33665c7
Ashish Behera <39208674+ashishbehera111@users.noreply.github.com>
2021-10-04 16:19:18 +0530

    hii
    hii

---
+++ 2-api (GET) in main page/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import POSTLIST from "./components/PostList"
+ class app extends Component {
+    render() {
+        return (
+            <div>
+               <POSTLIST/>
+            </div>
+        )
+    }
+}
+
+export default app
